004e788ea3a50e448ccee8575118f798d6234c87
rafa2024 <holaM@gmail.com>
2024-10-01 11:26:43 -0500

    Proyecto maven v-3

---
+++ src/main/java/model/TblUsuario.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the tbl_usuario database table.
+ * 
+ */
+@Entity
+@Table(name="tbl_usuario")
+@NamedQuery(name="TblUsuario.findAll", query="SELECT t FROM TblUsuario t")
+public class TblUsuario implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int idusuario;
+
+
+	private String password;
+
+	private String usuario;
+
+	public int getIdusuario() {
+		return idusuario;
+	}
+
+	public void setIdusuario(int idusuario) {
+		this.idusuario = idusuario;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	
+	
+
+
+
+	
+}
